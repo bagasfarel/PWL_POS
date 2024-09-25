c92e682af95793b9adefc2ec3990bf20c3ef2f29
bagas farel andrean <bagasfarel45@gmail.com>
2024-09-25 19:45:35 +0700

    Upload Jobsheet 5 Praktikum 2

--- resources/views/layouts/template.blade.php
+++ resources/views/layouts/template.blade.php
@@ -13,42 +13,48 @@
   <link rel="stylesheet" href="{{ asset('adminlte/dist/css/adminlte.min.css') }}">
 </head>
 <body class="hold-transition sidebar-mini">
+<!-- Site wrapper -->
+<div class="wrapper">
+  <!-- Navbar -->
+  @include('layouts.header')
   <!-- /.navbar -->
-  <div class="wrapper">
-    @include('layouts.header')
 
   <!-- Main Sidebar Container -->
   <aside class="main-sidebar sidebar-dark-primary elevation-4">
     <!-- Brand Logo -->
-        <a href="{{ url('/') }}" class="brand-link">
-        <img src="{{ assets('adminlte/dist/img/AdminLTELogo.png') }}" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
-      <span class="brand-text font-weight-light">PWL - Stater Code</span>
+    <a href="{{ url('/') }}" class="brand-link">
+      <img src="{{ asset('adminlte/dist/img/AdminLTELogo.png') }}" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
+      <span class="brand-text font-weight-light">PWL - Starter Code</span>
     </a>
-    <!-- Sidehar -->
-        @include('layouts.sidebar')
-        <!-- /.sidebar -->
-        </aside>
-        
-        <!-- Content Wrapper. Contains page content -->
-        <div class="content-wrapper">
-        <!-- Content Header (Page header)-->
-        @include('layouts.breadcrumb')
-        
-        <!-- Main content -->
-        <section class="content">
-            @yield('content')
-        </section>
-        <!-- /.content -->
-        </div>
-        <!-- /.content-wrapper -->
-        
-        @include('layouts.footer')
-    </div>
+
+    <!-- Sidebar -->
+    @include('layouts.sidebar')
+    <!-- /.sidebar -->
+  </aside>
+
+  <!-- Content Wrapper. Contains page content -->
+  <div class="content-wrapper">
+    <!-- Content Header (Page header) -->
+    @include('layouts.breadcrumb')
+
+    <!-- Main content -->
+    <section class="content">
+      @yield('content')
+    </section>
+    <!-- /.content -->
+  </div>
+  <!-- /.content-wrapper -->
+
+  @include('layouts.footer')
+</div>
+<!-- ./wrapper -->
+
 <!-- jQuery -->
 <script src="{{ asset('adminlte/plugins/jquery/jquery.min.js') }}"></script>
 <!-- Bootstrap 4 -->
 <script src="{{ asset('adminlte/plugins/bootstrap/js/bootstrap.bundle.min.js') }}"></script>
 <!-- AdminLTE App -->
 <script src="{{ asset('adminlte/dist/js/adminlte.min.js') }}"></script>
+<!-- AdminLTE for demo purposes -->
 </body>
 </html>
