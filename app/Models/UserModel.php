890639dbf157e686d29bfe01a70ffa19f0c75a9d
bagas farel andrean <bagasfarel45@gmail.com>
2024-10-28 09:51:27 +0700

    Upload JS 10 Praktikum 1

--- app/Models/UserModel.php
+++ app/Models/UserModel.php
@@ -7,9 +7,9 @@ use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Foundation\Auth\User as Authenticatable;
+use Tymon\JWTAuth\Contracts\JWTSubject;
 
-
-class UserModel extends Authenticatable
+class UserModel extends Authenticatable implements JWTSubject
 {
     use HasFactory;
 
@@ -43,5 +43,13 @@ class UserModel extends Authenticatable
     public function getRole()
     {
         return $this->level->leve_kode;
+    } 
+    public function getJWTIdentifier()
+    {
+        return $this->getKey();
+    }
+    public function getJWTCustomClaims()
+    {
+        return [];
     }
 }
